ce8084070506730b623b552a642fc9550c037c60
My Name <foo@lisp.us.to>
2017-01-28 21:20:07 -0500

    Adding objs.mk for C4-OS build system

---
+++ objs.mk
@@ -0,0 +1,13 @@
+# c4-os-specific library build rules
+
+minift-src = $(wildcard $(LIBRARY_ROOT)/miniforth/src/*.c)
+minift-obj = $(minift-src:.c=.o)
+
+$(BUILD)/libs/miniforth.a: $(minift-obj)
+	ar rvs $@ $^
+
+.PHONY: minift-clean
+minift-clean:
+	rm -f $(minift-obj)
+
+ALL_CLEAN += minift-clean
